5a69e45aa49529fdfed54f561399169e00ccf1d0
israel-force <oladosuisraelolayinka@gmail.com>
2021-01-08 11:09:43 +0100

    added router basename

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./styles/index.scss";
-import App from "./App";
-import { BrowserRouter as Router } from "react-router-dom";
-import ContextProvider from "./context/Context";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/index.scss';
+import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
+import ContextProvider from './context/Context';
 
 ReactDOM.render(
   <React.StrictMode>
     <ContextProvider>
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <App />
       </Router>
     </ContextProvider>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
