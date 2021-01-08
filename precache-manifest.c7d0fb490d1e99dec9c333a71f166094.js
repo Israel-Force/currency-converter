ed050ec42655cb36d930cb3ba6888255402f2d4e
israel-force <oladosuisraelolayinka@gmail.com>
2021-01-08 10:44:42 +0100

    Updates

---
+++ precache-manifest.c7d0fb490d1e99dec9c333a71f166094.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a807f62b710c6096b42ff2b374f4681",
+    "url": "/currency-converter/index.html"
+  },
+  {
+    "revision": "fb1954e9cf0fabab1d5b",
+    "url": "/currency-converter/static/css/main.80feaf01.chunk.css"
+  },
+  {
+    "revision": "8d1e8585aa0d7f8df39b",
+    "url": "/currency-converter/static/js/2.08608146.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/currency-converter/static/js/2.08608146.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb1954e9cf0fabab1d5b",
+    "url": "/currency-converter/static/js/main.d7598ae5.chunk.js"
+  },
+  {
+    "revision": "2aeb21825094dc2561f4",
+    "url": "/currency-converter/static/js/runtime-main.1b4db780.js"
+  },
+  {
+    "revision": "bc09aa84d9e5a263de9d80a4d9b67c3b",
+    "url": "/currency-converter/static/media/AvertaDemoPE.bc09aa84.otf"
+  },
+  {
+    "revision": "638dab656e6ab7ba2043996d2281b1a7",
+    "url": "/currency-converter/static/media/AvertaDemoPERegular.638dab65.otf"
+  },
+  {
+    "revision": "bc09aa84d9e5a263de9d80a4d9b67c3b",
+    "url": "/currency-converter/static/media/AvertaDemoPEboldItalic.bc09aa84.otf"
+  },
+  {
+    "revision": "f5331cb6372b6c0d8baf2dd7e200498c",
+    "url": "/currency-converter/static/media/OpenSansBold.f5331cb6.ttf"
+  },
+  {
+    "revision": "49f89e34d03233b1f27788f75df7a40a",
+    "url": "/currency-converter/static/media/OpenSansExtraBold.49f89e34.ttf"
+  },
+  {
+    "revision": "bf243bcbf81560535385c47725f57dfe",
+    "url": "/currency-converter/static/media/OpenSansItalic.bf243bcb.ttf"
+  },
+  {
+    "revision": "9ff12f694e5951a6f51a9d63b05062e7",
+    "url": "/currency-converter/static/media/OpenSansLight.9ff12f69.ttf"
+  },
+  {
+    "revision": "d7d5d4588a9f50c99264bc12e4892a7c",
+    "url": "/currency-converter/static/media/OpenSansRegular.d7d5d458.ttf"
+  },
+  {
+    "revision": "e1c83f9474e0cc1d84a13c6d1ddf3ca5",
+    "url": "/currency-converter/static/media/OpenSansSemiBold.e1c83f94.ttf"
+  },
+  {
+    "revision": "f557a575220bd528cb63ebbd75480afb",
+    "url": "/currency-converter/static/media/avatar.f557a575.woff"
+  },
+  {
+    "revision": "09b384914ccb1654d2727386e33db3bb",
+    "url": "/currency-converter/static/media/bg-2.09b38491.png"
+  },
+  {
+    "revision": "3743e35ed787b84af56132056fcf166e",
+    "url": "/currency-converter/static/media/logo.3743e35e.png"
+  },
+  {
+    "revision": "44443fb203e72de60b45167decae9f8a",
+    "url": "/currency-converter/static/media/thumb.44443fb2.png"
+  }
+]);
